e2be82d447d140ecba8ae49a9c365e5ebcd5b41f
Alisher <alisheroromov473@gmail.com>
2023-02-12 07:34:23 +0500

    I updated script.js

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,10 @@
 let numRnd = Math.floor(Math.random()*100)
+console.log(numRnd)
 let input = document.querySelector(`input`);
 
 let btn = document.querySelector(`.btn`);
 let todoList = document.querySelector(`.tab__todo`)
+let countChance = 0
 
 btn.addEventListener(`click`,function(){
     if(input.value != ``){
@@ -12,13 +14,24 @@ btn.addEventListener(`click`,function(){
         if(input.value>numRnd) {
            todoList.innerHTML += `
             <li style = 'font-family:sans-serif;'>Kiritgan soningiz katta, kichikroq son kiriting!</li>` 
+            countChance+=1
         }else if(input.value<numRnd) {
             todoList.innerHTML += `
             <li style = 'font-family:sans-serif;'>Kiritgan soningiz kichik, kattaroq son kiriting!</li>`
-        }else if(input.value==numRnd){
-            todoList.innerHTML += `
-            <li style = 'color: #fff;font-size: 20px;font-family: sans-serif;'>Barakalla siz topdingiz !!!</li>`
+            countChance+=1
 
+        }else if(input.value==numRnd){
+            countChance+=1
+
+            todoList.innerHTML += `
+            <li style = 'color: #fff;font-size: 20px;font-family: sans-serif;'>
+                Barakalla siz topdingiz !!! <br>
+                Sizning urinishlaringiz soni: ${countChance}
+            </li>`
+            input.setAttribute(`readonly`,`The End`)
+
+            console.log(todoList)
+            
         }
         
         
